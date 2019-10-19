321be14b290ffcbae3ea55c296e8f9981f1e5774
James lariviere <james.lariviere@bryanuniversity.edu>
2019-10-18 18:29:50 -0700

    javascript

---
+++ assignments/fsw105/week1/grocery_storejs/grocery.js
@@ -0,0 +1,10 @@
+var shopper = {
+firstName: 'Joe',
+lastName: 'Dawn',
+age: 21,
+rewardsMember: true,
+
+fullName () { return (firstName + '' + lastName);},
+groceryCart: ['noodles','broccoli','soysauce','tofu'],}
+
+console.log (shopper)
