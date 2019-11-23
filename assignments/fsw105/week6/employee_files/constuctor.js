a591a3deaa5d3d6becf4062b630a7c152b4cb0cb
James lariviere <james.lariviere@bryanuniversity.edu>
2019-11-22 22:41:19 -0800

    1

--- assignments/fsw105/week6/employee_files/constuctor.js
+++ assignments/fsw105/week6/employee_files/constuctor.js
@@ -1,1 +1,14 @@
 employees = []
+let status= "Full Time"
+function Employee(name,jobTitle,salery,status){
+    this.name = name
+    this.jobTitle = jobTitle
+    this.salery = salery
+    this.status = status
+    this.printEmployeeForm = function() {
+        console.log(Employee)  
+    }
+}
+const Rick = new Employee ("Rick","Scientist","3000 Flerbos","Contract");
+const James = new Employee("James","Workerbee",$150000/yr,);
+const Carrie = new type("carrie","Boss",$1000/hr);
