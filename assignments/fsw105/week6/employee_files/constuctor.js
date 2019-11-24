fa11d377f3cc87f6f61e05fab93fdc6c9f662d93
James lariviere <james.lariviere@bryanuniversity.edu>
2019-11-23 20:08:07 -0800

    constructor

--- assignments/fsw105/week6/employee_files/constuctor.js
+++ assignments/fsw105/week6/employee_files/constuctor.js
@@ -1,14 +1,19 @@
 employees = []
-let status= "Full Time"
+
 function Employee(name,jobTitle,salery,status){
     this.name = name
     this.jobTitle = jobTitle
     this.salery = salery
     this.status = status
-    this.printEmployeeForm = function() {
-        console.log(Employee)  
+      
     }
-}
+   printEmployeeForm = function() {
+       
+        return (new Employee) }
+
+let status= "Full Time"
 const Rick = new Employee ("Rick","Scientist","3000 Flerbos","Contract");
-const James = new Employee("James","Workerbee",$150000/yr,);
-const Carrie = new type("carrie","Boss",$1000/hr);
+const James = new Employee("James","Workerbee","$150000/yr",status);
+const Carrie = new Employee("carrie","Boss","$1000/hr",status);
+employees.push(Rick,James,Carrie)
+console.log(employees)
