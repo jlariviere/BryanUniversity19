97f05c2f6344c90d11df5317e6c4a01a513bebe5
James lariviere <james.lariviere@bryanuniversity.edu>
2019-11-24 10:53:58 -0800

    half

--- assignments/fsw110/week6/workbook/keypress/app.js
+++ assignments/fsw110/week6/workbook/keypress/app.js
@@ -3,3 +3,14 @@
 Example of output: You've pressed the "a" key. It's key code is 65.
 
 Wes Bos made this https://keycode.info/ for us to use as an example. */
+
+body.onkeydown = function(e) {
+    if (!e.metaKey) {
+      e.preventDefault();
+    }
+    drawNumberToCanvas(e.keyCode);
+  
+    // Main e.keyCode display
+    document.querySelector('.keycode-display').innerHTML = e.keyCode;}
+
+
--- assignments/fsw110/week6/workbook/submit/app.js
+++ assignments/fsw110/week6/workbook/submit/app.js
@@ -1,3 +1,13 @@
 // The "submit" event refreshes your browser by default
 // use "e.preventDefault()" in the first line of your callback function to prevent this
 // remember to pass "e" into the callback function
+document.getElementById("name").setAttribute("user_name")
+const form = document.submitMe
+document.getElementById("submitMe")
+event.preventDefault()
+form.addEventListener("clickMe"), (event) => {
+    event.preventDefault()
+
+
+
+}
