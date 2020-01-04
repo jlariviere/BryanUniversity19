fd6343fc1029db393e9cc494bfd91d8d349055d5
James lariviere <james.lariviere@bryanuniversity.edu>
2020-01-04 11:13:40 -0800

    week3-1

--- assignments/fsw115/week3/pokemon.js
+++ assignments/fsw115/week3/pokemon.js
@@ -7,16 +7,22 @@ xhr.onreadystatechange = function(){
         const JSONdata = xhr.responseText
        // console.log(xhr.responseText)
        const data = JSON.parse(JSONdata)
-       showData(data.objects)
-         console.log(data.objects)
+       showData (data.objects)
+         //console.log(data.objects)
     } 
 }
 
-
 function showData(arr){
     for(let i = 0; i < arr.length; i++){
-        const h1 = document.createElement('h1')
-        h1.textContent = arr[i].pokemon
-        document.body.appendChild(h1) 
+        for( let n=0; n<arr[i].pokemon.length; n++){
+        const values = Object.values(arr[i].pokemon[n])
+        console.log(values)
+    
+        const li = document.createElement("li")
+        li.textContent = values
+        document.body.appendChild(li) 
+      }  
     }
 }
+
+
